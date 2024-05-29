6f4c098f90bef39a446649dfe7a2d76500faf3b6
Swastik Dhar <swastikdhar18@gmail.com>
2024-05-29 14:17:52 +0530

    Navbar making...

--- client/app/components/Navbar.jsx
+++ client/app/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/logo.png"
 
 const Navbar = () => {
   return (
     <div>
-      
+      <Link href={"/"}>
+        <Image
+          src={logo}
+          alt="Picture of the author"
+          height={100}
+          width={100}
+        />
+      </Link>
+      <div>
+        {
+          <h1>Hallo</h1>
+        }
+        <button>upload pdf</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
